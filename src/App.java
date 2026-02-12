4aaf033f46c928e3efa119e6e64180a3b4cf174d
umer <syedumerahmedcode@gmail.com>
2026-02-12 06:53:27 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+        
+        '.' Matches any single character.​​​​
+        '*' Matches zero or more of the preceding element.
+        The matching should cover the entire input string (not partial).
+        
+        
+        
+        Example 1:
+        
+        Input: s = "aa", p = "a"
+        Output: false
+        Explanation: "a" does not match the entire string "aa".
+        Example 2:
+        
+        Input: s = "aa", p = "a*"
+        Output: true
+        Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+        Example 3:
+        
+        Input: s = "ab", p = ".*"
+        Output: true
+        Explanation: ".*" means "zero or more (*) of any character (.)".
+        
+         */
+        String s = "ab", p = ".*";
+        Solution solution = new Solution();
+        boolean result = solution.isMatch(s, p);
+        System.out.println("The result for the input s: "+s+" and pattern p: "+p+". Is the regular expression matching: "+result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,50 @@
 public class Solution {
 
     public boolean isMatch(String s, String p) {
+        /**
+         * Solved using dynamic programming
+         */
+        if (s == null || p == null) {
+            return true;
+        }
+
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        
+        //Base case
+        dp[0][0] = true;
+
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                // checking three different possibilities
+                // '.' scenario
+                if (p.charAt(j) == '.') {
+                    // we get diagonal value
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                // charcater scenario
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                // '*' scenario
+                if (p.charAt(j) == '*') {
+                    //'.''*' scenario
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        // the three scenarios that are possible
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+
         
     }
 
